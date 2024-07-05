a6a45d94057193905379bd60cf85468da8e95418
Jesse Anderson <jesse.anderson@gmail.com>
2024-07-05 15:39:11 -0700

    working example

--- ee_kfold/__init__.py
+++ ee_kfold/__init__.py
@@ -1,36 +1,35 @@
 import ee
 
 
-def sets(
-    training_data: ee.FeatureCollection, k: int = 10, seed: int | float = 1337
-) -> ee.List:
-    test_proportion = ee.Number(1.0 / k)
+def sets(training_data: ee.FeatureCollection, k: int = 10, seed: int | float = 1337):
+    test_proportion = 1.0 / k
     training_data = training_data.randomColumn("r", seed)
 
-    return ee.List.sequence(
-        0, ee.Number(1).subtract(test_proportion), test_proportion
-    ).map(
-        lambda lower_limit: (
+    return [
+        (
             training_data.filter(
                 ee.Filter.Or(
-                    ee.Filter.lt("r", lower_limit),
-                    ee.Filter.gte("r", ee.Number(lower_limit).add(test_proportion)),
+                    ee.Filter.lt("r", i * test_proportion),
+                    ee.Filter.gte("r", (i + 1) * test_proportion),
                 )
             ),
             training_data.filter(
                 ee.Filter.Or(
-                    ee.Filter.gte("r", lower_limit),
-                    ee.Filter.lt("r", ee.Number(lower_limit).add(test_proportion)),
+                    ee.Filter.gte("r", i * test_proportion),
+                    ee.Filter.lt("r", (i + 1) * test_proportion),
                 )
             ),
         )
-    )
+        for i in range(k)
+    ]
 
 
 def run_kfold(model: ee.Classifier, training_data: ee.FeatureCollection, **kwargs):
     results = []
-    for train, test in sets(training_data, **kwargs):
-        trained_model = model.train(features=train)
-        results.append(test.classify(trained_model))
+    for train, test in sets(training_data):
+        trained_model = model.train(features=train, **kwargs)
+        train_result = train.classify(trained_model)
+        test_result = test.classify(trained_model)
+        results.append((trained_model, train_result, test_result))
 
     return results
--- test/test_set.py
+++ test/test_set.py
@@ -1,14 +1,32 @@
+from statistics import fmean
+
 import ee
-from ee_kfold import sets
+from ee_kfold import sets, run_kfold
+import pytest
 
 ee.Initialize(project="ncs-gc")
 
 
-def test_sets():
-    img = ee.Image().constant(1)
+@pytest.fixture
+def training_data() -> ee.FeatureCollection:
+    slope = ee.Terrain.slope(ee.Image("USGS/3DEP/10m"))
+    y = slope.lt([5, 10, 20]).reduce("sum").rename("y")
+    X = slope.add(ee.Image.random().multiply(15)).rename("X")
     region = ee.Geometry.Rectangle([-112, 34, -111, 35])
-    training_data = img.sample(region, numPixels=100)
-    s = sets(training_data).getInfo()
+    return X.addBands(y).sample(region, numPixels=100)
+
+
+def test_sets(training_data):
+    s = sets(training_data)
     assert isinstance(s, list)
 
-def test_kfold
+
+def test_kfold(training_data):
+    model = ee.Classifier.smileRandomForest(10)
+    results = run_kfold(model, training_data, classProperty="y", inputProperties=["X"])
+
+    mean_accuracy = fmean(
+        [r[2].errorMatrix("y", "classification").accuracy().getInfo() for r in results]
+    )
+    print(mean_accuracy)
+    breakpoint()
